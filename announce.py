c30992a96d93c86c3670d49b4b34d8ac2be5bb13
Hugo Wainwright <wainwrighthugo@gmail.com>
2018-01-04 15:06:14 +0000

    plug streams if player on win streak is streaming

--- announce.py
+++ announce.py
@@ -9,23 +9,25 @@ import urllib.request
 from typing import Tuple
 
 import pyrebase
-from pyrebase.pyrebase import Database
 
+TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID", "")
 ANNOUNCEMENT_CHANNEL_ID = os.getenv("ANNOUNCEMENT_CHANNEL_ID", "219804933916983296")
 ANNOUNCE_URL = os.getenv("ANNOUNCE_URL", "http://localhost:40862")
 FIREBASE_CONFIG = os.getenv("FIREBASE_CONFIG", "")
 WIN_STREAKS_CACHE_FILE = os.getenv("WIN_STREAKS_CACHE_FILE", "win_streaks.cache")
 WIN_STREAK_MESSAGES = {
-    4: "<@{}> is on a 4 win streak!",
-    6: "Killing spree! <@{}> is on a 6 win streak!",
-    8: "RAMPAGE. <@{}> is on an 8 win streak!",
+    3: "<@{}> is on a 3 game win streak!",
+    4: "<@{}> is on a 4 game win streak!",
+    6: "Killing spree! <@{}> is on a 6 game win streak!",
+    8: "RAMPAGE. <@{}> is on an 8 game win streak!",
     9: "<@{}> is completely dominating with a 9 win streak!",
     10: "U N S T O P P A B L E. <@{}> is on a 10 win streak!",
     15: "🎉 🎉 🎉 🎉 Congratulations! <@{}> has gone 15 games without losing a single one! 🎉 🎉 🎉 🎉"
 }
 
 
-def fetch_win_streaks(db: Database, member: str) -> Tuple[str, int]:
+def fetch_win_streaks(member: str) -> Tuple[str, int]:
+    db = create_db_connection()
     regions = db.child("members").child(member).child("characters").get().val()
     if not regions:
         regions = {}
@@ -45,12 +47,20 @@ def fetch_win_streaks(db: Database, member: str) -> Tuple[str, int]:
     return member, max(character_win_streaks, default=0)
 
 
-def announce_win_streak(member: str, streak: int) -> None:
+def announce_win_streak(member: str, streak: int, previous_streak: int) -> None:
+    stream_data = fetch_stream_data(member)
+    if streak <= previous_streak or (streak < 4 and not stream_data) or streak < 3:
+        return
+
     if streak in WIN_STREAK_MESSAGES:
         data = {
             "channel_id": ANNOUNCEMENT_CHANNEL_ID,
             "message": WIN_STREAK_MESSAGES.get(streak).format(member)
         }
+
+        if stream_data.get("name", "") and stream_data.get("type", "") == "live":
+            stream_name = stream_data["name"]
+            data["message"] += "\nTune in to https://www.twitch.tv/{} and show your support!".format(stream_name)
         try:
             urllib.request.urlopen(ANNOUNCE_URL, data=json.dumps(data).encode("utf-8"))
         except urllib.request.URLError as e:
@@ -58,28 +68,57 @@ def announce_win_streak(member: str, streak: int) -> None:
             print(e)
 
 
-def main():
-    db_config = pickle.loads(gzip.decompress(base64.b64decode(FIREBASE_CONFIG)))
-    db = pyrebase.initialize_app(db_config).database()
+def fetch_stream_data(member: str) -> dict:
+    db = create_db_connection()
+    twitch_connection = db.child("members").child(member).child("connections").child("twitch").get().val()
+    if not twitch_connection or not twitch_connection.get("id", "") or not twitch_connection.get("name", ""):
+        return {}
 
+    url = "https://api.twitch.tv/helix/streams?first=1&user_id={}".format(twitch_connection["id"])
+    request = urllib.request.Request(url, headers={"Client-ID": TWITCH_CLIENT_ID})
+    try:
+        response = urllib.request.urlopen(request)
+        stream_data = json.loads(response.read().decode("utf-8"))
+
+        if stream_data.get("data", []):
+            result = stream_data["data"][0]
+            result["name"] = twitch_connection["name"]
+            return result
+
+    except urllib.request.URLError as e:
+        print(e)
+
+    return {}
+
+
+def main():
+    db = create_db_connection()
     members = db.child("members").shallow().get().val()
     if not members:
         members = []
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
-        futures, _ = concurrent.futures.wait([executor.submit(fetch_win_streaks, db, member) for member in members])
+        futures, _ = concurrent.futures.wait([executor.submit(fetch_win_streaks, member) for member in members])
         win_streaks = [future.result() for future in futures if future.done()]
 
     if os.path.exists(WIN_STREAKS_CACHE_FILE):
         with open(WIN_STREAKS_CACHE_FILE, "rb") as file:
             win_streaks_cache = dict(pickle.load(file))
 
-        for member, streak in win_streaks:
-            if streak > win_streaks_cache.get(member, 0):
-                announce_win_streak(member, streak)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+            concurrent.futures.wait([
+                executor.submit(announce_win_streak, member, streak, win_streaks_cache.get(member, 0))
+                for member, streak
+                in win_streaks])
 
     with open(WIN_STREAKS_CACHE_FILE, "wb") as file:
         pickle.dump(win_streaks, file)
+
+
+def create_db_connection():
+    db_config = pickle.loads(gzip.decompress(base64.b64decode(FIREBASE_CONFIG)))
+    db = pyrebase.initialize_app(db_config).database()
+    return db
 
 
 if __name__ == "__main__":
